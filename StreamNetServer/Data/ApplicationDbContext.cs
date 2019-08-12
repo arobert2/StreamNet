7f292e47bb928e7d7ed9e4a066e1d61bf456a161
Allen Roberts <allen.roberts1985@gmail.com>
2019-08-11 22:35:36 -0400

    Add project files.

---
+++ StreamNetServer/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StreamNetServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StreamNetServer.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+        /// <summary>
+        /// Represents your video library
+        /// </summary>
+        public DbSet<VideoMetaData> Videos { get; set; }
+        /// <summary>
+        /// Represents your music library
+        /// </summary>
+        public DbSet<AudioMetaData> Music { get; set; }
+    }
+}
---
+++ StreamNetServer/Models/IMediaMetaData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StreamNetServer.Models
+{
+    interface IMediaMetaData
+    {
+        //Media Description Properties
+        string Title { get; set; }
+        string Subtitle { get; set; }
+        int Rating { get; set; }
+        string Comments { get; set; }
+
+        //Media File Properties
+        string Name { get; set; }
+        string FolderPath { get; set; }
+        DateTime DateCreated { get; set; }
+        DateTime DateModified { get; set; }
+        double Size { get; set; }
+    }
+}
